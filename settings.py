202f12903cf78a90f84277888c610074149c5f01
zerasul <zerasul@gmail.com>
2020-02-26 20:57:32 +0100

    adding minimal configuration

---
+++ settings.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+BASE_DIR = Path('.').resolve()
+
+# Templates directory
+templateDir = str(BASE_DIR / 'templates')
+
+# Posts directory
+postDir = str(BASE_DIR / 'posts')
+
+# Default layout template
+defaultLayout = "template.html"
+
+# Static files directory
+staticDir = str(BASE_DIR / 'static')
+
+# Website title
+title = 'Blask | A Simple Blog Engine Based on Flask'
+
+errors= { 404 : "404"
+        }
